abda7be830982ff9744899e2fd4d5ba552d4e0ae
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2024-06-03 22:09:37 +0200

    Create propertyInsertion.sql

---
+++ SQL Code/propertyInsertion.sql
@@ -0,0 +1,8 @@
+INSERT INTO Property (PropertyId, BuildingId, OwnerId, PropertyType, PropertyLocation, PropertyStatus, RentalPrice)
+VALUES (1, 1, 1, 'apartment', 'Geschwister-Scholl-Strasse', 'habited', 320);
+
+INSERT INTO Property (PropertyId, BuildingId, OwnerId, PropertyType, PropertyLocation, PropertyStatus, RentalPrice)
+VALUES (2, 1, 2, 'apartment', 'Geschwister-Scholl-Strasse', 'habited', 320);
+
+INSERT INTO Property (PropertyId, BuildingId, OwnerId, PropertyType, PropertyLocation, PropertyStatus, RentalPrice)
+VALUES (3, 1, 3, 'apartment', 'Geschwister-Scholl-Strasse', 'habited', 320);
