5b0d29a0c083dcc617160ac7f4a3ff13c49ddbdb
luciabg13 <119361249+luciabg13@users.noreply.github.com>
2024-06-03 22:10:27 +0200

    Create ownerInsertion.sql

---
+++ SQL Code/ownerInsertion.sql
@@ -0,0 +1,3 @@
+INSERT INTO Owners (OwnerId, OwnerInformation, OwnerName) VALUES (1, 'Information 1', 'Dominik');
+INSERT INTO Owners (OwnerId, OwnerInformation, OwnerName) VALUES (2, 'Information 2', 'Gabriel');
+INSERT INTO Owners (OwnerId, OwnerInformation, OwnerName) VALUES (3, 'Information 3', 'Dirk');
